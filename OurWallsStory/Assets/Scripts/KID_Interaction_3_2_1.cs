49d729a9282f3666807317a2f2d8b82f8976f804
Rémi Moreaux <moreaux_rmi@yahoo.fr>
2020-10-01 09:37:44 +0200

    POLISH and OPTIMISATION

--- OurWallsStory/Assets/Scripts/KID_Interaction_3_2_1.cs
+++ OurWallsStory/Assets/Scripts/KID_Interaction_3_2_1.cs
@@ -8,20 +8,31 @@ public class KID_Interaction_3_2_1 : MonoBehaviour
     public GameObject Interaction;
     public GameObject House;
     public GameObject Canvas;
+    public GameObject Flora;
 
     private Animator Interaction_Animator;
     private Animator House_Animator;
+    private Animator Flora_Animator;
     private bool PauseActivated;
+    public bool FloraLaunch;
 
     void Start()
     {
         Interaction_Animator = Interaction.GetComponent<Animator>();
         House_Animator = House.GetComponent<Animator>();
+        Flora_Animator = Flora.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (FloraLaunch == true)
+        {
+            Flora_Animator.SetBool("Launch", true);
+            FloraLaunch = false;
+        }
+        
         PauseActivated = Canvas.GetComponent<Pause_Menu>().PauseActivated;
         Vector3 CamPos = Camera.main.transform.position;
 
